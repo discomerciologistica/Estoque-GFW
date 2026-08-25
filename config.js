8d581f6aacafaa9e07b7340f51cc15dfff3d17a4
discomerciologistica <admlogistica@discomercio.com.br>
2026-08-25 19:54:12 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// Cole aqui a URL do seu Web App do Apps Script (Implantar > Nova implantação)
+const API_URL = "https://script.google.com/macros/s/AKfycbwhF7voUAscnM2VfbT1uNX53tA3KP2vv7WUsgzY4l8PjMbXcKIFwuEuDVOIbuH0jofR/exec";
